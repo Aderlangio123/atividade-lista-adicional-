b70d3440925614bea0ec4eee73dc6aea78619864
Aderlangio123 <faderlangio123@gmail.com>
2024-09-16 08:49:57 -0300

    atividade lista adicinal

--- lista.py
+++ lista.py
@@ -1,15 +1,14 @@
 #criar lista e exibir nomes colocados pelo usuário!
 
-# Passo 1: Crie uma lista vazia
 lista_de_nomes = []
 
-# Passo 2: Adicione 5 nomes à lista
+
 lista_de_nomes.append(input("digite 1° nome da lista:   "))
 lista_de_nomes.append(input("digite 2° nome da lista:   "))
 lista_de_nomes.append(input("digite 3° nome da lista:   "))
 lista_de_nomes.append(input("digite 4° nome da lista:   "))
 lista_de_nomes.append(input("digite 5° nome da lista:   "))
 
-# Exiba a lista para verificar
+
 print("sua lista contém os nomes: ", " " ,lista_de_nomes)
 
